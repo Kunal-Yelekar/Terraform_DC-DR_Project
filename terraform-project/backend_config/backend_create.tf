423247eac39ec028499f0eb173d66e08f8c3889f
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-04-14 01:20:28 +0530

    Create backend_create.tf

---
+++ terraform-project/backend_config/backend_create.tf
@@ -0,0 +1,43 @@
+# tfbackend.tf
+#############################
+
+# Create an S3 bucket for Terraform remote state storage
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-unique-terraform-state-bucket-knl2"  # Ensure this bucket name is globally unique.
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "TerraformStateBucket"
+    Environment = terraform.workspace
+  }
+}
+
+# Optional: Create a DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "TerraformLockTable"
+    Environment = terraform.workspace
+  }
+}

f8855a6d608e7580264d7f6086bf424354cf6887
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-04-14 01:33:37 +0530

    Update backend_create.tf

--- terraform-project/backend_config/backend_create.tf
+++ terraform-project/backend_config/backend_create.tf
@@ -11,13 +11,22 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  tags = {
+    Name        = "TerraformStateBucket"
+    Environment = terraform.workspace
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_sse" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
+
 
   tags = {
     Name        = "TerraformStateBucket"
